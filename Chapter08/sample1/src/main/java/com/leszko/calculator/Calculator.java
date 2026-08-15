adfd87c6cb6fe1c23c0247151c7c9a76431bf7fd
densonajd <ajdenson2300@gmail.com>
2026-08-15 13:23:30 -0400

    Update Calculator.java to add Javadoc comment

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,6 +3,10 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+/**
+ * Allows the calculator to add numbers
+ */
+
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
